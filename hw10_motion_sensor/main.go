03b369188a60b34a4316731e8c350287ab323ea8
ddpakhom <ddpakhom@gmail.com>
2024-06-30 13:27:19 +0300

    HW10 is completed

--- hw10_motion_sensor/main.go
+++ hw10_motion_sensor/main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func SensorReadings() <-chan int {
+	c := make(chan int)
+	go func() {
+		for {
+			c <- rand.Intn(100)
+			time.Sleep(time.Millisecond * 600)
+		}
+	}()
+	return c
+}
+
+func ProcessReadings(c <-chan int) <-chan float64 {
+	processed := make(chan float64)
+	go func() {
+		var sum int
+		var count int
+		for reading := range c {
+			sum += reading
+			count++
+			if count == 10 {
+				processed <- float64(sum) / 10
+				sum = 0
+				count = 0
+			}
+		}
+	}()
+	return processed
+}
+
 func main() {
-	// Place your code here.
+	readings := SensorReadings()
+	processed := ProcessReadings(readings)
+
+	startTime := time.Now()
+	iterations := 0
+
+	for time.Since(startTime) < time.Minute {
+		fmt.Println(<-processed)
+		iterations++
+	}
+	fmt.Println("Минута прошла. Количество выполненных итераций:", iterations)
 }
